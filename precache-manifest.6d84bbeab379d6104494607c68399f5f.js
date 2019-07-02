4378c8918532d0eb50326349ba312b2594ac98c8
Adnan Muhib <adnan.muhib@rocketmail.com>
2019-07-02 11:34:51 +0500

    Updates

---
+++ precache-manifest.6d84bbeab379d6104494607c68399f5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a55f18b208e12691308f6746d2b475",
+    "url": "/Blog_Reaction/index.html"
+  },
+  {
+    "revision": "576b61c83d754b6a1d9e",
+    "url": "/Blog_Reaction/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1c8ec1f358f6fd90269f",
+    "url": "/Blog_Reaction/static/js/2.35343102.chunk.js"
+  },
+  {
+    "revision": "576b61c83d754b6a1d9e",
+    "url": "/Blog_Reaction/static/js/main.719265c7.chunk.js"
+  },
+  {
+    "revision": "139679d6be153ef3f95b",
+    "url": "/Blog_Reaction/static/js/runtime~main.3c4b503b.js"
+  }
+]);
